f0eabd01a3300338a9cd5b0edba7217d2235e760
pretty7in <pretty7in@gmail.com>
2023-10-02 15:41:59 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-// Get the modal element and button to open it
-const modal = document.getElementById('contactModal');
-const openModalBtn = document.getElementById('openModalBtn');
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the modal element and button to open it
+    const modal = document.getElementById('contactModal');
+    const openModalLink = document.querySelector('a[href="#contactModal"]');
 
-// Get the close button inside the modal
-const closeBtn = document.querySelector('.close');
+    // Get the close button inside the modal
+    const closeBtn = document.querySelector('.close');
 
-// Open the modal when the button is clicked
-openModalBtn.addEventListener('click', () => {
-    modal.style.display = 'block';
-});
+    // Open the modal when the link is clicked
+    openModalLink.addEventListener('click', (e) => {
+        e.preventDefault(); // Prevent the default behavior of the anchor link
+        modal.style.display = 'block';
+    });
 
-// Close the modal when the close button is clicked
-closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
-
-// Close the modal when the background outside the modal is clicked
-window.addEventListener('click', (event) => {
-    if (event.target === modal) {
+    // Close the modal when the close button is clicked
+    closeBtn.addEventListener('click', () => {
         modal.style.display = 'none';
-    }
+    });
+
+    // Close the modal when the background outside the modal is clicked
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
 });
